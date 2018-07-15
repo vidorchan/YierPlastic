0d594eb61b6d295d891d85869d1566b26ac59c1d
vidorchan <1282911204@qq.com>
2018-07-15 15:11:56 +0800

    certificates image update

--- src/views/Certificates.vue
+++ src/views/Certificates.vue
@@ -4,8 +4,9 @@
         <div id="certificates" class="pageCon">
             <el-tabs type="border-card">
                 <el-tab-pane v-bind:label="$t('message.certificates').toUpperCase()">
-
-                    <p><img src=""/>图片</p>
+                    <p v-for="cert in certificates">
+                        <img v-bind:src="cert" style="width: 100%"/>
+                    </p>
                 </el-tab-pane>
             </el-tabs>
         </div>
@@ -17,7 +18,16 @@
         name: 'Certificates',
         data() {
             return {
-
+                certificates: [
+                    '/static/images/certificates/cer1.jpg',
+                    '/static/images/certificates/cer2.jpg',
+                    '/static/images/certificates/cer3.jpg',
+                    '/static/images/certificates/cer4.jpg',
+                    '/static/images/certificates/cer5.jpg',
+                    '/static/images/certificates/cer6.jpg',
+                    '/static/images/certificates/cer7.jpg',
+                    '/static/images/certificates/cer8.jpg'
+                ]
             }
         }
     }
