ee7432edcbc3d8647507a2bc9b015c555ba28df7
vidorchan <1282911204@qq.com>
2018-07-18 22:49:32 +0800

    size in product details page

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -14,46 +14,57 @@
                             </p>
                         </el-tab-pane>
                         <el-tab-pane :label="$t('product.params')" name="second">
-                            <dl>
-                                <dt>{{$t('product.material')}}:</dt>
-                                <dd>{{$t('product.materialV')}}</dd>
-                            </dl>
-                            <dl v-if="product.params.plasticType && product.params.plasticType != ''">
-                                <dt>{{$t('product.plasticType')}}:</dt>
-                                <dd v-text="$t(product.params.plasticType)"></dd>
-                            </dl>
-                            <dl v-if="product.params.industrialUse && product.params.industrialUse != ''">
-                                <dt>{{$t('product.industrialUse')}}:</dt>
-                                <dd v-text="$t(product.params.industrialUse)"></dd>
-                            </dl>
-                            <dl v-if="product.params.use && product.params.use != ''">
-                                <dt>{{$t('product.use')}}:</dt>
-                                <dd v-text="$t(product.params.use)"></dd>
-                            </dl>
-                            <dl v-if="product.params.sealingType && product.params.sealingType != ''">
-                                <dt>{{$t('product.sealingType')}}:</dt>
-                                <dd v-text="$t(product.params.sealingType)"></dd>
-                            </dl>
-                            <dl v-if="product.params.surfaceHandling && product.params.surfaceHandling != ''">
-                                <dt>{{$t('product.surfaceHandling')}}:</dt>
-                                <dd v-text="$t(product.params.surfaceHandling)"></dd>
-                            </dl>
-                            <dl>
-                                <dt>{{$t('product.place')}}:</dt>
-                                <dd>{{$t('product.placeV')}}</dd>
-                            </dl>
-                            <dl>
-                                <dt>{{$t('product.brand')}}:</dt>
-                                <dd>{{$t('product.brandV')}}</dd>
-                            </dl>
-                            <dl v-if="product.params.modelN && product.params.modelN != ''">
-                                <dt>{{$t('product.modelN')}}:</dt>
-                                <dd v-text="product.params.modelN"></dd>
-                            </dl>
-                            <dl v-if="product.params.color && product.params.color != ''">
-                                <dt>{{$t('product.color')}}:</dt>
-                                <dd v-text="$t(product.params.color)"></dd>
-                            </dl>
+                            <el-table :data="product.size" v-if="product.size" border>
+                                <el-table-column label="Capacity(容量)" prop="capacity" header-align="center" align="center" class-name="test">
+                                </el-table-column>
+                                <el-table-column label="Height(瓶身高-最高)/mm" prop="height" header-align="center" align="center">
+                                </el-table-column>
+                                <el-table-column label="Diameter(瓶身直径-最宽)/mm" prop="diameter" header-align="center" align="center">
+                                </el-table-column>
+                            </el-table>
+                            <div style="clear: both; height: 20px;"></div>
+                            <div>
+                                <dl>
+                                    <dt>{{$t('product.material')}}:</dt>
+                                    <dd>{{$t('product.materialV')}}</dd>
+                                </dl>
+                                <!--<dl v-if="product.params.plasticType && product.params.plasticType != ''">-->
+                                    <!--<dt>{{$t('product.plasticType')}}:</dt>-->
+                                    <!--<dd v-text="$t(product.params.plasticType)"></dd>-->
+                                <!--</dl>-->
+                                <!--<dl v-if="product.params.industrialUse && product.params.industrialUse != ''">-->
+                                    <!--<dt>{{$t('product.industrialUse')}}:</dt>-->
+                                    <!--<dd v-text="$t(product.params.industrialUse)"></dd>-->
+                                <!--</dl>-->
+                                <!--<dl v-if="product.params.use && product.params.use != ''">-->
+                                    <!--<dt>{{$t('product.use')}}:</dt>-->
+                                    <!--<dd v-text="$t(product.params.use)"></dd>-->
+                                <!--</dl>-->
+                                <!--<dl v-if="product.params.sealingType && product.params.sealingType != ''">-->
+                                    <!--<dt>{{$t('product.sealingType')}}:</dt>-->
+                                    <!--<dd v-text="$t(product.params.sealingType)"></dd>-->
+                                <!--</dl>-->
+                                <!--<dl v-if="product.params.surfaceHandling && product.params.surfaceHandling != ''">-->
+                                    <!--<dt>{{$t('product.surfaceHandling')}}:</dt>-->
+                                    <!--<dd v-text="$t(product.params.surfaceHandling)"></dd>-->
+                                <!--</dl>-->
+                                <dl>
+                                    <dt>{{$t('product.place')}}:</dt>
+                                    <dd>{{$t('product.placeV')}}</dd>
+                                </dl>
+                                <dl>
+                                    <dt>{{$t('product.brand')}}:</dt>
+                                    <dd>{{$t('product.brandV')}}</dd>
+                                </dl>
+                                <dl v-if="product.params.modelN && product.params.modelN != ''">
+                                    <dt>{{$t('product.modelN')}}:</dt>
+                                    <dd v-text="product.params.modelN"></dd>
+                                </dl>
+                                <!--<dl v-if="product.params.color && product.params.color != ''">-->
+                                    <!--<dt>{{$t('product.color')}}:</dt>-->
+                                    <!--<dd v-text="$t(product.params.color)"></dd>-->
+                                <!--</dl>-->
+                            </div>
                         </el-tab-pane>
                     </el-tabs>
                 </el-col>
@@ -84,7 +95,7 @@
             fetchDate() {
                 var c_id = this.$route.query.cid;
                 var p_id = this.$route.query.pid;
-                this.$http.get('/static/json/' + this.$i18n.locale + '/products.json').then((response) => {
+                this.$http.get('/static/json/en/products.json').then((response) => {
                     // 请求成功回调
                     var productList;
                     if (c_id != '') {
@@ -121,6 +132,9 @@
         },
         mounted() {
             this.fetchDate();
+            goTop: {
+                window.scrollTo(0,0);
+            }
         }
     }
 </script>
@@ -155,5 +169,14 @@
     .el-tab-pane dt {
         width: 100px;
         color: #666;
+    }
+
+    .el-table tr, .el-table th {
+        background: #dce8f5;
+    }
+
+    .el-table--border {
+        border: 3px solid #f0f9eb;
+        border-radius: 10px;
     }
 </style>
